a026e18fc7a29a06330fbff509a438fa20a7fa1f
蕾米莉亚 <332161863@qq.com>
2019-10-13 16:55:34 +0800

    search/tab栏切换样式

--- pages/search/index.js
+++ pages/search/index.js
@@ -5,9 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabNav: ["推荐", "明星", "颜值", "摄影", "运动", "美食", "旅行", "时尚", "萌宠", "家居", "美妆", "育儿",],
+    // tabNav点击后的索引值
+    currentIndex: 0,
   },
 
+
+  // tabNav栏点击事件
+  clicktabNavItem(e) {
+    console.log(e.target.dataset.index)
+    const {
+      index
+    } = e.target.dataset
+    this.setData({
+      currentIndex: index,
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
